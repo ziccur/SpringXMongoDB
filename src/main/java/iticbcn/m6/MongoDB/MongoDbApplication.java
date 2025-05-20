9c1be59a492910ec44ccfff6251fb0de7837461f
Yeray <2023_yeray.reyes@iticbcn.cat>
2025-05-20 20:43:40 +0200

    Initial commit with dependencies

---
+++ src/main/java/iticbcn/m6/MongoDB/MongoDbApplication.java
@@ -0,0 +1,13 @@
+package iticbcn.m6.MongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbApplication.class, args);
+	}
+
+}
