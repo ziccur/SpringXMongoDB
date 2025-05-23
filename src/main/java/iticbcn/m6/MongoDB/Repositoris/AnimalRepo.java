6b45ec5402418b5b2bc0ff8805eb1933037a2ac8
Yeray <2023_yeray.reyes@iticbcn.cat>
2025-05-23 17:55:38 +0200

    add custom regex method

--- src/main/java/iticbcn/m6/MongoDB/Repositoris/AnimalRepo.java
+++ src/main/java/iticbcn/m6/MongoDB/Repositoris/AnimalRepo.java
@@ -1,13 +1,12 @@
 package iticbcn.m6.MongoDB.Repositoris;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import org.springframework.stereotype.Repository;
 
 import iticbcn.m6.MongoDB.Model.Animal;
+import reactor.core.publisher.Flux;
 
 @Repository
-@EnableReactiveMongoRepositories
 public interface AnimalRepo extends ReactiveMongoRepository<Animal, String> {
-    
+    Flux<Animal> findByNomRegex(String regex);
 }
